9acad0093a707b67c097b399ec5aa47bb90133c2
luizzz.z127@gmail.com <aluno@IPIRANGA.FATEC>
2023-03-09 16:26:14 -0300

    apostila02(oo): bloco de codigo 2.3.1 encerrado

--- Personagem.java
+++ Personagem.java
@@ -1,23 +1,48 @@
+import static java.lang.Math.min;
 public class Personagem{
     String nome;
-    int energia;
-    int fome;
-    int sono;
+    int energia = 10 ;
+    int fome = 0;
+    int sono = 0;
 
     /*Isso é um metodo de instância */
     void cacar(){
         System.out.println(nome + "cacando");
+        if (energia >= 2){
+            System.out.println(nome + "cacando");
+            //energia = energia -2;
+            energia -= 2;
+        }
+        else{
+            System.out.println(nome + "sem energia para cacar");
+        }
+        fome = Math.min(fome +1 , 10);
     }
 
     void comer(){
-        System.out.print(nome + "comendo\n");
-
+        if(fome >= 1){
+            System.out.print(nome + "comendo\n");
+            energia = min(energia + 1,10);
+            --fome;
+        }
+        else{
+            System.out.println(nome + "Sem fome ");
+        }
     }
 
     void dormir(){
+        if(sono >= 1){//nãp tem como ficar menor que por conta dessa verficação 
         System.out.printf("%s dormindo%n");
+        energia = energia + 1 < 10?energia + 1 : 10;
+
+        sono--;
+        }
     }
 
+
+
+
+    
     /*Para pular linha no windowns tem que colocar /r/n  no linux é \n , para pular linha idempendente da plataforma, coloca-se %n */
 }
 /*Classe com auto coesão -> classe faz somente uma coisa */
@@ -25,6 +50,14 @@ public class Personagem{
 
 /*A letra A significa: que foi adicionado e está pronto para fazer um commmit
   A letra M significa que o arquivo foi modificado
- Quando os arquivos estão verdes significa que esta em staged(pronto para um commit)
- A letra U significa que esta Untracked: não esta sendo monitorado pelo git 
+  Quando os arquivos estão verdes significa que esta em staged(pronto para um commit)
+  A letra U significa que esta Untracked: não esta sendo monitorado pelo git 
+  -------------------------------------
+  Não existe uma posição de memória "vazia"
+
+
+  git diff 
+
+
+  estado = coleção de valores que caracteriza o objeto naquele momento 
 */
--- TestePersonagem.java
+++ TestePersonagem.java
@@ -3,10 +3,18 @@ public class TestePersonagem {
         Personagem p = new Personagem(); /*p é uma variavel de referencia(um ponteiro)  OBJETO p*/
         //java 10+
         /*var p = new Personagem;*/
-
+        //int idade =  18;
+        
+       //System.out.println(idade >= 18? "Sim": "nao");
         p.nome = "John";
         p.cacar();
         p.comer();
         p.dormir();
+
+        
     }
+
+
+   
 }
+
